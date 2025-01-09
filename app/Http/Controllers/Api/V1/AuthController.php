afcff83c157b1a13c72778117e68fbc928cded76
Shafi <msh.shafiul@gmail.com>
2025-01-09 22:40:15 +0600

    Add user_id to login response for enhanced user identification

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -24,6 +24,7 @@ class AuthController extends BaseController
             $user = Auth::user();
             $success['token'] = $user->createToken('auto_token')->plainTextToken;
             $success['name'] = $user->name;
+            $success['user_id'] = $user->id;
             $success['role_id'] = $user->role_id;
             $success['team'] = $user->team;
 
