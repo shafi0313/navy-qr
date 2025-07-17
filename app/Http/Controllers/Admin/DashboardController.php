1efeb9f85f5808ce0466ba3038bc67bf910e6c9f
shafi0313 <msh.shafiul@gmail.com>
2025-07-17 22:00:08 +0600

    Fix order of menu names in appInstructions query

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -69,7 +69,7 @@ class DashboardController extends Controller
 
             $data = ['data' => $data];
         }
-        $data['appInstructions'] = AppInstruction::orderByRaw("FIELD(menu_name, 1,2,3.4,5,15,6,7,8,9,10,14,11,12,14)")->get();
+        $data['appInstructions'] = AppInstruction::orderByRaw("FIELD(menu_name, 1,2,3.4,5,6,7,8,15,9,10,14,11,12,14)")->get();
 
         return view('admin.dashboard', $data);
     }
