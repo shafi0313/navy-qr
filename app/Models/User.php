63bc3b013d0f0fdc15a6c09581b8587d80b08440
shafi0313 <msh.shafiul@gmail.com>
2025-07-17 18:41:27 +0600

    Remove Spatie roles trait from User model and delete unused configuration files: Clean up codebase

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,11 +8,10 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, HasRoles, Notifiable, SoftDeletes;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
