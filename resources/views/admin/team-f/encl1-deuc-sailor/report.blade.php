3d49ca270149a31f414e3168c61acded01307177
shafi0313 <msh.shafiul@gmail.com>
2025-08-13 15:10:24 +0600

    Update PDF export button labels for Encl-1 and Encl-2 reports

--- resources/views/admin/team-f/encl1-deuc-sailor/report.blade.php
+++ resources/views/admin/team-f/encl1-deuc-sailor/report.blade.php
@@ -7,71 +7,13 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
-                    {{-- <form action="{{ route('admin.reports.daily_state.report') }}">
-                        <div class="row justify-content-center">
-                            <div class="col-md-12 text-center my-3">
-                                <h4>Daily Recruitment State Selection Page</h4>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="mb-3 mx-1 row">
-                                    <div class="col-sm-4">
-                                        <label for="start_date" class="col-sm-2 col-form-label required text-nowrap">Date
-                                            Form </label>
-                                    </div>
-                                    <div class="col-sm-8">
-                                        <input type="date" name="start_date" value="{{ $startDate }}" class="form-control" id="start_date"
-                                            required>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="mb-3 mx-1 row">
-                                    <div class="col-sm-4">
-                                        <label for="end_date" class="col-sm-2 col-form-label required text-nowrap">Date To
-                                        </label>
-                                    </div>
-                                    <div class="col-sm-8">
-                                        <input type="date" name="end_date" value="{{ $endDate }}" class="form-control" id="end_date" required>
-                                    </div>
-                                </div>
-                            </div>
-                            @if (user()->role_id == 1)
-                                <div class="col-md-3">
-                                    <div class="mb-3 mx-1 row">
-                                        <div class="col-sm-4">
-                                            <label for="date" class="col-sm-2 col-form-label required text-nowrap">Team
-                                            </label>
-                                        </div>
-                                        <div class="col-sm-8">
-                                            <select name="team" class="form-select" required>
-                                                <option value="">Select Team</option>
-                                                <option value="all" @selected($team=='all')>All</option>
-                                                <option value="A" @selected($team=='A')>A</option>
-                                                <option value="B" @selected($team=='B')>B</option>
-                                                <option value="C" @selected($team=='C')>C</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-                            @endif
-                            <div class="col-md-2 text-center">
-                                <div class="mb-3 row justify-content-center">
-                                    <label for="date" class="visually-hidden">Team</label>
-                                    <button type="submit" class="btn btn-primary" style="width: 100px">Submit</button>
-                                </div>
-                            </div>
-                        </div>
-                    </form> --}}
-
                     <div class="card-header">
                         <a href="{{ route('admin.team_f.encl1_deuc_sailor.report', 'pdf') }}" class="btn btn-primary">
-                            Export to PDF
+                            Export Encl-1
                         </a>
                     </div>
                     <hr>
-
                     @include('admin.team-f.encl1-deuc-sailor.table', ['applications' => $applications])
-
                 </div> <!-- end card-body -->
             </div> <!-- end card -->
         </div><!-- end col -->
