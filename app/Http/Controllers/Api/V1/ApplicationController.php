c261a42f2e879ceac7ed2956ad1c32236a44e109
shafi0313 <msh.shafiul@gmail.com>
2025-11-21 16:12:07 +0600

    Rename variable for clarity in counting today's scanned applicants by user

--- app/Http/Controllers/Api/V1/ApplicationController.php
+++ app/Http/Controllers/Api/V1/ApplicationController.php
@@ -211,7 +211,7 @@ class ApplicationController extends BaseController
             }
         } elseif (user()->exam_type == ExamType::OFFICER) {
             $data['allApplicationsByUser'] = ApplicationUrl::where('user_id', user()->id)->count();
-            $data['todayApplicationsByUser'] = ApplicationUrl::where('user_id', user()->id)->whereDate('scanned_at', now())->count();
+            $data['todayScannedApplicantsByUser'] = ApplicationUrl::where('user_id', user()->id)->whereDate('scanned_at', now())->count();
         }
 
         return $this->sendResponse($data, 'Applicants count.');
